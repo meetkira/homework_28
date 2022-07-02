7ac05ba87c17d1f55094e9250a4e412a5f947269
Кристина Ратник <kristi.2001@yandex.ru>
2022-07-02 14:25:28 +0300

    add ad views

--- ads/urls.py
+++ ads/urls.py
@@ -4,4 +4,8 @@ from django.urls import path
 from ads import views
 
 urlpatterns = [
+    path('', views.index),
+    path('ad/', views.AdListView.as_view()),
+    path("ad/<int:pk>/", views.AdDetailView.as_view()),
+    path("ad/create/", views.AdCreateView.as_view()),
 ]
--- ads/views.py
+++ ads/views.py
@@ -2,10 +2,13 @@ import json
 import os
 
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView, CreateView, ListView
 
 from ads.models import Ad, Category
 from users.models import Location, User
@@ -13,3 +16,80 @@ from users.models import Location, User
 
 def index(request):
     return JsonResponse({"status": "ok"}, status=200)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdListView(ListView):
+    model = Ad
+    queryset = Ad.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+
+        response = []
+        for ad in self.object_list:
+            response.append({
+                "id": ad.id,
+                "name": ad.name,
+                "price": ad.price,
+                "description": ad.description,
+                "is_published": ad.is_published,
+                "image": ad.image.url if ad.image else None,
+                "author": ad.author.first_name,
+                "category": ad.category.name,
+            })
+
+        return JsonResponse(response, safe=False)
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdCreateView(CreateView):
+    model = Ad
+    fields = ["name", "price", "description", "is_published", "author", "category"]
+
+    def post(self, request, *args, **kwargs):
+        ad_data = json.loads(request.body)
+
+        author = get_object_or_404(User, pk=ad_data["author_id"])
+        category = get_object_or_404(Category, pk=ad_data["category_id"])
+        try:
+            ad = Ad.objects.create(
+                name=ad_data["name"],
+                price=ad_data["price"],
+                description=ad_data.get("description", None),
+                is_published=ad_data["is_published"],
+                author=author,
+                category=category
+            )
+
+            return JsonResponse({
+                "id": ad.id,
+                "name": ad.name,
+                "price": ad.price,
+                "description": ad.description,
+                "is_published": ad.is_published,
+                "image": ad.image.url if ad.image else None,
+                "author": ad.author.first_name,
+                "category": ad.category.name,
+            }, safe=False, status=200)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=403)
+
+
+class AdDetailView(DetailView):
+    model = Ad
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        self.object = self.get_object()
+
+        return JsonResponse({
+            "id": self.object.id,
+            "name": self.object.name,
+            "price": self.object.price,
+            "description": self.object.description,
+            "is_published": self.object.is_published,
+            "image": self.object.image.url if self.object.image else None,
+            "author": self.object.author.first_name,
+            "category": self.object.category.name,
+        })
