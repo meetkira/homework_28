120f8db90b79dd2057494dce535988e7811b131a
Кристина Ратник <kristi.2001@yandex.ru>
2022-07-29 12:53:47 +0300

    add names to urls

--- info/urls.py
+++ info/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from info import views
 
 urlpatterns = [
-    path('import_info/', views.AddInfo.as_view()),
-    path('', views.index),
+    path('import_info/', views.AddInfo.as_view(), name='info'),
+    path('', views.index, name='index'),
 ]
